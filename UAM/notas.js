11f541344212bedd7228358907d952cebbae6bad
CaueCS <cfr@csglobalit.com>
2021-09-23 22:09:50 -0300

    23/09 ATV III

---
+++ UAM/notas.js
@@ -0,0 +1,30 @@
+
+// function Cadastro() {
+//     var nome_cliente = document.getElementById("nome").value;
+//     var endereco_cliente = document.getElementById("endereco").value;
+//     var idade = document.getElementById("idade").value;
+//     if (document.getElementById("masculino").checked){
+//     var sexo_cliente = “M”
+
+// var dadosajax = {
+// 'nome_cliente': nome_cliente,
+// ‘endereco_cliente’: endereço_cliente,
+// ‘idade_cliente’: idade_cliente,
+// ‘sexo_cliente’: sexo_cliente
+// };
+// var pageurl = grava_cliente.php';
+// $.ajax({
+// url: pageurl,
+// data: dadosajax,
+// type: 'POST',
+// cache: false,
+// error: function(XMLHttpRequest, textStatus, errorThrown)
+// {
+// alert(“Erro Gravação Cliente!!”);
+// },
+// success: function(result)
+// {
+// alert("Gravação Cliente efetuada com sucesso");
+// return;
+// }
+// }
